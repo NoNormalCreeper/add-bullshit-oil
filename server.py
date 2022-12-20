e093e2abeb60b0bf0377af49ff7571d1e215911b
NoNormalCreeper <2560359315@qq.com>
2022-12-20 15:09:52 +0800

    ✨ web page

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import fastapi
+from fastapi.responses import HTMLResponse
 import uvicorn
 from bullshit_oil_generator import generate_bullshit_oil
 
@@ -7,3 +8,8 @@ app = fastapi.FastAPI()
 @app.get("/bullshit_oil")
 async def _(length: int = 80, name: str = None, class_: str = None, event: str = None):
     return generate_bullshit_oil(length, name, class_, event)
+
+
+@app.get("/", response_class=HTMLResponse)
+async def _():
+    return open("index.html", encoding="utf-8").read()
